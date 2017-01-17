513bb93762df6f949036bbe2252ec80eccf0e530
Michael Widlok <michaelwidlok@yahoo.de>
2017-01-17 19:16:41 +0100

    OnClick Listener hinzugefuegt

--- app/src/main/java/com/example/mwidlok/masteringandroidapplication/MainActivity.java
+++ app/src/main/java/com/example/mwidlok/masteringandroidapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.mwidlok.masteringandroidapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,19 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Button btn1 = (Button) findViewById(R.id.btnGo);
+
+		btn1.setOnClickListener(new View.OnClickListener()
+		{
+			public void onClick(View v)
+			{
+				doSometing();
+			}
+		});
 	}
+
+
 
 	public void doSometing()
 	{

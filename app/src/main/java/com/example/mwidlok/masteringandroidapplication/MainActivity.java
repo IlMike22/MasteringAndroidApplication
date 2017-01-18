c8072dd5542649f45ad93c3a4940efc9b88664f5
MWidlok <m.widlok@immowelt.de>
2017-01-18 16:33:22 +0100

    Button raus, Default Fragment inital rein

--- app/src/main/java/com/example/mwidlok/masteringandroidapplication/MainActivity.java
+++ app/src/main/java/com/example/mwidlok/masteringandroidapplication/MainActivity.java
@@ -1,36 +1,19 @@
 package com.example.mwidlok.masteringandroidapplication;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
 import layout.ContactFragment;
 
 public class MainActivity extends AppCompatActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		Button btn1 = (Button) findViewById(R.id.btnGo);
-
-		btn1.setOnClickListener(new View.OnClickListener()
-		{
-			public void onClick(View v)
-			{
-				doSometing();
-			}
-		});
-	}
-
-	public void doSometing()
-	{
-		Log.i("info", "oh yeah");
 		ContactFragment contactFragment = ContactFragment.newInstance();
 		getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, contactFragment).commit();
-
 	}
 }

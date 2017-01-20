55923ee55d943715007b615c15d9bc0bea9616a5
MWidlok <m.widlok@immowelt.de>
2017-01-20 17:28:50 +0100

    PageAdapter Implementation Start

---
+++ app/src/main/java/com/example/mwidlok/masteringandroidapplication/classes/MyPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.example.mwidlok.masteringandroidapplication.classes;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import layout.ListFragment;
+import layout.ContactFragment;
+import layout.SettingsFragment;
+
+/**
+ * Created by MWidlok on 20.01.2017.
+ */
+
+public class MyPagerAdapter extends FragmentPagerAdapter {
+
+	MyPagerAdapter(FragmentManager fm)
+	{
+		super(fm);
+	}
+
+	@Override
+	public Fragment getItem(int position) {
+		switch(position)
+		{
+			case 0:
+				return new ListFragment();
+			case 1:
+				return new ContactFragment();
+			case 2:
+				return new SettingsFragment();
+			default:
+				return null;
+		}
+	}
+
+	@Override
+	public int getCount() {
+		return 3;
+	}
+}
